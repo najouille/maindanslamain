0d5ef6a00c1ce564918bc696968162354882eac7
flavie25 <flavie.dussaillant@gmail.com>
2020-12-11 11:34:26 +0100

    partenaires

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,1 +1,11 @@
+const closeButton = document.querySelector('.cookiesaccepter');
+const closeButton1 = document.querySelector('#quitter');
+const cookie = document.querySelector('#cookiesbox');
 
+closeButton.addEventListener('click', function () {
+    cookie.style.display = "none";
+});
+
+closeButton1.addEventListener('click', function () {
+    cookie.style.display = "none";
+});
